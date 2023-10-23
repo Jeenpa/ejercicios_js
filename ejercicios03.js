faab27952cb748a2ecf0b1e31cca11f7695a5e80
jeenpa <parra2089@gmail.com>
2023-10-22 20:54:27 -0400

    ejercicios resueltos 22/10/23

--- ejercicios03.js
+++ ejercicios03.js
@@ -7,19 +7,15 @@ numeroAleatorio();
 
 // 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
 
-const capicua = ( nmr )=>{
-    let inverso = parseInt(nmr.toString().split("").reverse().join(""));
+const capicua = (nmr = 0)=>{
+    let n = nmr.toString();
+    let inverso = n.split("").reverse().join("");
 
-    if(nmr === inverso){
-        console.log(`El numero ${nmr} si es capicúa`);
-    } else{
-        console.log(`El numero ${nmr} no es capicúa`);
-    }
+    return (n === inverso) ? console.log('si') : console.log('no');
 
 }
 
 capicua(2002);
-
 
 // 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
 
