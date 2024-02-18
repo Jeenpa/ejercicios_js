5495b3d7c482b1ea317cd378b8837fbf373eb7d0
jeenpa <parra2089@gmail.com>
2024-02-18 17:24:52 -0400

    cambios 18/02/24

--- dom/js/index_dom.js
+++ dom/js/index_dom.js
@@ -1,7 +1,10 @@
 import hamburgerMenu from "./menu_hamburguesa.js";
+import { alarm, digitalCLock } from "./reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     hamburgerMenu(".panel-btn", ".panel", ".menu a");
+    digitalCLock("#reloj", "#activar-reloj", "#desactivar-reloj");
+    alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
 });
